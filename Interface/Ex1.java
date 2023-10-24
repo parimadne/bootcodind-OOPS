c8772fdce5eaf568014ea22e47b8545dc9551496
parimadne <parimadne04@gmail.com>
2023-10-24 23:06:17 +0530

    Basic OOPs Java

---
+++ Interface/Ex1.java
@@ -0,0 +1,19 @@
+package Interface;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        A a1;
+        B b1 = new B();
+        b1.show();
+        a1 =b1;
+        a1.show();
+    }
+}
+interface A{
+    void show();
+}
+class B implements A{
+   public void show(){
+       System.out.println("I am in B");
+    }
+}
